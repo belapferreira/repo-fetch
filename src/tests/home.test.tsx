8b41ff45e3b4e1498b71a7eb7a147fc62a053ade
belapferreira <isabelapenhaferreira@gmail.com>
2026-07-04 14:41:27 -0300

    test: add tests for the Home page

---
+++ src/tests/home.test.tsx
@@ -0,0 +1,39 @@
+import { Home } from '@/pages/home/page';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter, RouterProvider } from 'react-router-dom';
+import { describe, expect, it } from 'vitest';
+import userEvent from '@testing-library/user-event';
+import { router } from '@/routes';
+
+const HomePage = () => {
+  return (
+    <MemoryRouter>
+      <Home />
+    </MemoryRouter>
+  );
+};
+
+const RouterProviderRender = () => {
+  return <RouterProvider router={router} />;
+};
+
+describe('Home page', () => {
+  it('should render without crashing', () => {
+    render(<HomePage />);
+
+    const title = screen.getByText('Explore repositórios do GitHub');
+
+    expect(title).toBeInTheDocument();
+  });
+
+  it('should navigate to the repositories list page after submitting the form', async () => {
+    const user = userEvent.setup();
+
+    render(<RouterProviderRender />);
+
+    await user.type(screen.getByPlaceholderText('Username do usuário no Github'), 'octocat');
+    await user.click(screen.getByRole('button', { name: /buscar/i }));
+
+    expect(router.state.location.pathname).toBe('/octocat');
+  });
+});
