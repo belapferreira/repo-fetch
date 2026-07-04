9624b9bd5eb16a1a0cd98152f0d4128348f91725
belapferreira <isabelapenhaferreira@gmail.com>
2026-07-04 13:19:23 -0300

    chore: update the way to import images

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,22 +1,19 @@
 import { Link } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 
+import logo from '@/assets/repo-fetch-logo.svg';
+import github from '@/assets/github.svg';
+
 export const Header = () => {
   return (
     <header className="navbar p-3 shadow-lg">
       <Container>
         <Link to="/" className="navbar-brand">
-          <img src="../src/assets/repo-fetch-logo.svg" width="114" height="36" alt="Repo Fetch" className="logo" />
+          <img src={logo} width="114" height="36" alt="Repo Fetch" className="logo" />
         </Link>
 
         <a href="https://github.com/belapferreira/repo-fetch" target="_blank" rel="noopener noreferrer">
-          <img
-            src="../src/assets/github.svg"
-            width="36"
-            height="36"
-            alt="Repositório no GitHub"
-            title="Repositório no GitHub"
-          />
+          <img src={github} width="36" height="36" alt="Repositório no GitHub" title="Repositório no GitHub" />
         </a>
       </Container>
     </header>
--- src/pages/not-found/page.tsx
+++ src/pages/not-found/page.tsx
@@ -1,5 +1,7 @@
 import { Card, Container } from 'react-bootstrap';
 
+import notFoundImage from '@/assets/not-found-404.svg';
+
 export const NotFound = () => {
   return (
     <section className="d-flex flex-column align-items-center py-5 h-100 flex-grow-1">
@@ -8,7 +10,7 @@ export const NotFound = () => {
           style={{ maxWidth: '40rem', marginInline: 'auto' }}
           className="p-5 gap-4 d-flex flex-column align-items-center border-0 mt-5 bg-secondary bg-opacity-10 shadow-sm rounded-3"
         >
-          <img src="/src/assets/not-found-404.svg" alt="404" width={194} height={85} />
+          <img src={notFoundImage} alt="404" width={194} height={85} />
 
           <h1 className="fs-3">Não encontrado</h1>
 
