b7b6cf9bfb98c5b83b65b88a3d00842bafcf27b0
belapferreira <isabelapenhaferreira@gmail.com>
2026-07-05 07:33:16 -0300

    feat: add OG image

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -4,7 +4,7 @@ import { Container } from 'react-bootstrap';
 export const Home = () => {
   return (
     <section className="d-flex flex-column align-items-center py-5 h-100 flex-grow-1">
-      <Container className="mb-5">
+      <Container className="mb-2 mb-md-5">
         <h1 className="h5">Explore repositórios do GitHub</h1>
         <p>Consultar repositórios hospedados no GitHub através de um username</p>
       </Container>
